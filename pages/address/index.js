937300260bc181cf9e9e62ca1c6563484ee54378
adilshaik <shaik.adil@hotmail.com>
2022-03-10 14:05:26 +0530

    Added index address

--- pages/address/index.js
+++ pages/address/index.js
@@ -1,8 +1,20 @@
-import { getSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 import React from 'react';
 
 const AddressIndex = () => {
-  return <h1>Hello</h1>;
+  const { data: session } = useSession();
+  return (
+    <main className='mt-5'>
+      <h1 className='text-center text-3xl font-semibold tracking-wide text-gray-800 my-5'>
+        Your Addresses
+      </h1>
+      <div className='max-w-7xl my-10 mx-auto '>
+        {session?.userDetails?.addresses.map((address) => (
+          <div className='border-2 grid gap-4 content-between w-full'></div>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export const getServerSideProps = async (context) => {
