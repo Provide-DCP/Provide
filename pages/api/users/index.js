452d64c58b74fb9f4b8fe45f08b1aa00f39fa495
Thusharke <thushar.ke25@gmail.com>
2022-03-10 10:11:08 +0530

    added location

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,15 +1,15 @@
-import connectDB from '../../../src/lib/connectDB.js';
-import userDetails from '../../../models/UserDetailsModel';
+import connectDB from "../../../src/lib/connectDB.js";
+import userDetails from "../../../models/UserDetailsModel";
 
 export default async function handler(req, res) {
   switch (req.method) {
-    case 'GET':
+    case "GET":
       await searchUserDetails(req, res);
       break;
-    case 'POST':
+    case "POST":
       await createUserDetails(req, res);
       break;
-    case 'PUT':
+    case "PUT":
       await updateUserDetails(req, res);
       break;
   }
@@ -19,24 +19,26 @@ const createUserDetails = async (req, res) => {
   try {
     await connectDB();
 
-    const { firstName, lastName, category, phone, userId } = req.body;
+    const { userId, firstName, lastName, image, category, phone, address } =
+      req.body;
 
     if (!userId) {
-      return res.status(400).json({ message: 'Invalid Credentials' });
+      return res.status(400).json({ message: "Invalid Credentials" });
     }
 
     const createUserDetails = new userDetails({
       user: userId,
       firstName,
       lastName,
+      image,
       category,
-      emailList: [email],
       phone,
+      address,
     });
 
     await createUserDetails.save();
     res.json({
-      message: 'Success! User Details Created',
+      message: "Success! User Details Created",
       details: createUserDetails,
     });
   } catch (error) {
@@ -50,7 +52,7 @@ const searchUserDetails = async (req, res) => {
     const userId = req.query.userId;
 
     if (!userId) {
-      return res.status(400).json({ message: 'Invalid Credentials' });
+      return res.status(400).json({ message: "Invalid Credentials" });
     }
 
     const details = await userDetails.findOne({
@@ -58,9 +60,9 @@ const searchUserDetails = async (req, res) => {
     });
 
     if (details) {
-      return res.status(200).json({ message: 'Details Found', details });
+      return res.status(200).json({ message: "Details Found", details });
     } else {
-      return res.status(200).json({ message: 'Details not found' });
+      return res.status(200).json({ message: "Details not found" });
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -73,7 +75,7 @@ const updateUserDetails = async (req, res) => {
     const { firstName, lastName, userId } = req.body;
 
     if (!userId) {
-      return res.status(400).json({ message: 'Invalid Credentials' });
+      return res.status(400).json({ message: "Invalid Credentials" });
     }
 
     const details = await userDetails.findOneAndUpdate(
@@ -85,9 +87,9 @@ const updateUserDetails = async (req, res) => {
       { new: true }
     );
     if (details) {
-      return res.status(200).json({ message: 'Details Updated', details });
+      return res.status(200).json({ message: "Details Updated", details });
     } else {
-      return res.status(200).json({ message: 'Please try again!' });
+      return res.status(200).json({ message: "Please try again!" });
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
