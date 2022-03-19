ad7a33902192ad3085aaab232d48746118914a56
Thusharke <thushar.ke25@gmail.com>
2022-03-19 22:07:41 +0530

    integrated producst with backend

--- pages/dashboard/provider/products/index.js
+++ pages/dashboard/provider/products/index.js
@@ -1,16 +1,77 @@
 import React from "react";
 import { ProductList } from "../../../../src/components/Shared/ProductList";
+import { getSession } from "next-auth/react";
+import axios from "axios";
 
-const ProductIndex = () => {
+const ProductIndex = ({ store, products }) => {
   return (
     <main className="md:ml-[14%] mt-[2%]">
       <section className="flex flex-col lg:flex-row justify-evenly mx-auto">
         <div className="w-full lg:w-11/12">
-          <ProductList />
+          <ProductList products={products} />
         </div>
       </section>
     </main>
   );
+};
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!session.userDetails) {
+    return {
+      redirect: {
+        destination: "/auth/user/details",
+        permanent: false,
+      },
+    };
+  }
+
+  const {
+    data: { store },
+  } = await axios.get("http://localhost:3000/api/store", {
+    params: {
+      userId: session.userId,
+    },
+  });
+  if (session.userDetails.category !== "provider" || !store) {
+    const category = session.userDetails.category;
+    return {
+      redirect: {
+        destination:
+          category === "customer" ? `/customer` : `/dashboard/${session.userDetails.category}`,
+        permanent: false,
+      },
+    };
+  }
+
+  let products = [];
+
+  if (store) {
+    const { data } = await axios.get("http://localhost:3000/api/products", {
+      params: {
+        storeId: store._id,
+      },
+    });
+    products = data.products;
+  }
+
+  return {
+    props: {
+      session,
+      store,
+      products,
+    },
+  };
 };
 
 export default ProductIndex;
--- src/components/Shared/GridView.js
+++ src/components/Shared/GridView.js
@@ -5,24 +5,24 @@ export const GridView = ({ products }) => {
   return (
     <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 xl:gap-x-5">
       {products.map((product) => (
-        <div key={product.id} className="group relative">
+        <div key={product._id} className="group relative">
           <div className="w-full h-48 bg-gray-200 aspect-w-2 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-48 lg:aspect-none">
             <img
-              src={product.imageSrc}
-              alt={product.imageAlt}
+              src={product.image}
+              alt={"image"}
               className="w-full h-full object-center object-cover lg:w-full lg:h-full"
             />
           </div>
           <div className="mt-2 px-2 items-center flex justify-between">
             <div>
               <h3 className="text-[18px] text-gray-700">
-                <a href={product.href}>
+                <a href={`/dashboard/provider/product/${product._id}`}>
                   <span aria-hidden="true" className="absolute inset-0" />
                   {product.name}
                 </a>
               </h3>
             </div>
-            <p className="text-md font-semibold text-indigo-900">{product.price}</p>
+            <p className="text-md font-semibold text-indigo-900">{"Rs. " + product.price}</p>
           </div>
         </div>
       ))}
