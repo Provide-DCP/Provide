159ccfa9986dbabcd02627ba8ddf3a4aab79a267
adilshaik <shaik.adil@hotmail.com>
2022-03-19 21:22:02 +0530

    Added dashboard

--- pages/dashboard/provider/index.js
+++ pages/dashboard/provider/index.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 import { EmptyState } from '../../../src/components/Provider/EmptyState';
 import Link from 'next/link';
 import { BiStore } from 'react-icons/bi';
+import { Stats } from '../../../src/components/Provider/Stats';
+import { Chart } from '../../../src/components/Provider/Chart';
 
 const flows = [
   { id: '01', name: 'Create Store', href: '#', status: 'current' },
@@ -55,17 +57,13 @@ const Index = ({ store, products }) => {
   ) {
     return (
       <>
-        <main className='flex-1 md:ml-64 '>
-          <div className='py-6'>
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
-              <h1 className='text-2xl font-semibold text-gray-900'>
-                Dashboard
-              </h1>
-            </div>
+        <main className='md:ml-[14%] mt-[2%] px-10 flex-1'>
+          <div>
             <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
               {/* Replace with your content */}
-              <div className='py-4'>
-                <div className='border-4 border-dashed border-gray-200 rounded-lg h-96' />
+              <div className='my-2'>
+                <Stats />
+                <Chart />
               </div>
               {/* /End replace */}
             </div>
