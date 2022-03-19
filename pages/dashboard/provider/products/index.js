c02f781d65377c3fa897ce290122dfb5b36ba107
adilshaik <shaik.adil@hotmail.com>
2022-03-19 13:21:36 +0530

    Changed products structure

--- pages/dashboard/provider/products/index.js
+++ pages/dashboard/provider/products/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { ProductList } from '../../../../../src/components/Shared/ProductList';
+import { ProductList } from '../../../../src/components/Shared/ProductList';
 
 const ProductIndex = () => {
   return (
-    <main className='ml-[15%] mt-[2%]'>
+    <main className='md:ml-[15%] mt-[2%]'>
       <section className='flex flex-col lg:flex-row justify-evenly max-w-7xl mx-auto'>
         <div className='w-full lg:w-11/12'>
           <ProductList />
