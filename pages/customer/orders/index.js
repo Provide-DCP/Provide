31da28cd1acadbcc0b522a1daa21bb522cf3af0d
adilshaik <shaik.adil@hotmail.com>
2022-03-20 14:28:28 +0530

    Updated order details page

--- pages/customer/orders/index.js
+++ pages/customer/orders/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { OrderDetailsCard } from '../../../src/components/Customer/OderDetailsCard';
 
 const Orders = () => {
-  return <div className='md:ml-[14%]'>Orders</div>;
+  return (
+    <main className='md:ml-[14%]'>
+      <h2 className='text-center my-10 text-4xl font-bold text-gray-600'>
+        Your Orders
+      </h2>
+      <div className='flex flex-col'>
+        <OrderDetailsCard />
+        <OrderDetailsCard />
+      </div>
+    </main>
+  );
 };
 
 export default Orders;
