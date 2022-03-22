91bbf6881cdcdd00e881ccbe23e0169edaa4acbf
Thusharke <thushar.ke25@gmail.com>
2022-03-22 14:41:55 +0530

    added request creation form

--- pages/customer/services/add.js
+++ pages/customer/services/add.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { FaCheckCircle } from "react-icons/fa";
+import { FaCheckCircle, FaPlus } from "react-icons/fa";
 import Loader from "../../../src/components/Layouts/Loader";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import Link from "next/link";
 const AddRequest = () => {
   const { data: session } = useSession();
   const router = useRouter();
+  const [active, setActive] = useState("");
   const [category, setCategory] = useState("");
   const [address, setAddress] = useState({});
   const [location, setLocation] = useState({
@@ -57,6 +59,7 @@ const AddRequest = () => {
       pending: true,
       category,
       location: address,
+      address,
       currentLocation: {
         latitude: location.coordinates.latitude,
         longitude: location.coordinates.longitude,
@@ -71,7 +74,7 @@ const AddRequest = () => {
       });
       if (rotp) {
         toast.success("Success, Request Created!", { toastId: "Success, Request Created!" });
-        router.push("/customer/orders");
+        router.push("/customer/services");
       }
     }
   };
@@ -80,7 +83,49 @@ const AddRequest = () => {
       <div>
         <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
       </div>
-      <div></div>
+      <div className="rounded-lg shadow w-full relative bg-white px-4 pt-14 pb-8 overflow-hidden sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+        <h1>Pick an address</h1>
+        <div className="my-10 mx-auto grid grid-cols-3 gap-4 w-full">
+          <Link href={"/customer/address/add"}>
+            <a className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 shadow-md rounded-md hover:bg-gray-50 w-80 h-64">
+              <FaPlus color="#ccc" size={35} />
+              <h1 className="mt-2 text-lg font-bold text-gray-400">Add Address</h1>
+            </a>
+          </Link>
+          {session?.userDetails?.addresses.map((address) => (
+            <div
+              onClick={() => {
+                setActive(active === address._id ? "" : address._id);
+                setAddress(active === address._id ? {} : address);
+              }}
+              key={address._id}
+              className={`${
+                active === address._id ? "border-2 border-indigo-400" : ""
+              } border rounded-md pb-4 px-5 shadow-md grid gap-4 content-between w-80 h-64 cursor-pointer`}
+            >
+              <div>
+                <h3 className="font-semibold py-3 text-center border-b-2">{address.name}</h3>
+                <p className="text-sm mt-4 font-semibold text-gray-600">
+                  {address.building.length >= 35
+                    ? `${address.building.slice(0, 35)}...`
+                    : address.building}
+                </p>
+
+                <p className="text-sm my-1 font-semibold text-gray-600">
+                  {address.area.length >= 35 ? `${address.area.slice(0, 35)}...` : address.area}
+                </p>
+                <p className="text-sm my-1 uppercase font-semibold text-gray-600">
+                  {address.city}, {address.region} {address.pincode}
+                </p>
+                <p className="text-sm my-1 font-semibold text-gray-600">{address.country}</p>
+                <p className="text-sm my-1 font-semibold text-gray-600">
+                  Phone Number: {address.phone}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="mt-4 grid grid-cols-6 gap-6 border-t py-5">
         <div className="col-span-6 sm:col-span-3">
           <label htmlFor="latitude" className="block text-sm font-medium text-gray-700">
