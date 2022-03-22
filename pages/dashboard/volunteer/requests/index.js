2244df0024ad14a0ac0cd75a7945d8f1f82ea59f
Thusharke <thushar.ke25@gmail.com>
2022-03-22 15:00:01 +0530

    added request

--- pages/dashboard/volunteer/requests/index.js
+++ pages/dashboard/volunteer/requests/index.js
@@ -1,8 +1,56 @@
+import axios from "axios";
+import { getSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
-const index = () => {
-  return <>handle Requests</>;
+const Index = ({ requests }) => {
+  console.log(requests);
+  return <p className="mt-20">handle Requests</p>;
 };
 
-export default index;
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!session.userDetails) {
+    return {
+      redirect: {
+        destination: "/auth/user/details",
+        permanent: false,
+      },
+    };
+  }
+
+  const {
+    data: { requests },
+  } = await axios.get(process.env.HOST_URL + "/api/requests");
+
+  if (session.userDetails.category !== "volunteer") {
+    const category = session.userDetails.category;
+    return {
+      redirect: {
+        destination:
+          category === "customer"
+            ? `/customer/stores`
+            : `/dashboard/${session.userDetails.category}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      requests,
+    },
+  };
+};
+
+export default Index;
