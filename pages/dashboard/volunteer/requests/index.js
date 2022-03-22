b053599a5388dbafbea78d8810135d1fd4c4688d
Thusharke <thushar.ke25@gmail.com>
2022-03-22 15:04:03 +0530

    added handle

--- pages/dashboard/volunteer/requests/index.js
+++ pages/dashboard/volunteer/requests/index.js
@@ -1,10 +1,24 @@
 import axios from "axios";
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const Index = ({ requests }) => {
   console.log(requests);
+  const { data: session } = useSession();
+  const handleAcceptRequest = async (req) => {
+    try {
+      const { data: request } = await axios.put("/api/requests", {
+        request: {
+          ...req,
+          pending: false,
+          volunteer: session.userId,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return <p className="mt-20">handle Requests</p>;
 };
 
