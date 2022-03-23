6a369bc8d12863beced580f736787e5f78e14d39
adilshaik <shaik.adil@hotmail.com>
2022-03-23 11:22:28 +0530

    Added verify

--- pages/auth/verify.js
+++ pages/auth/verify.js
@@ -4,10 +4,10 @@ import { useSession } from "next-auth/react";
 
 export default function Example() {
   return (
-    <div className="text-center mt-48">
-      <h3 className="mt-2 text-sm font-medium text-gray-900">No projects</h3>
-      <p className="mt-1 text-sm text-gray-500">
-        Get started by creating a new project.
+    <div className='text-center mt-48'>
+      <h3 className='mt-2 text-sm font-medium text-gray-900'>Verification Pending...</h3>
+      <p className='mt-1 text-sm text-gray-500'>
+        Please check you registered email for verification link.
       </p>
     </div>
   );
