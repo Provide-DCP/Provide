ae8c9b37fcb0395ae7c9be6ebb27bcf0d0cf2d9a
Thusharke <thushar.ke25@gmail.com>
2023-04-13 01:03:46 +0530

    minor chagnes

--- pages/dashboard/provider/products/add.js
+++ pages/dashboard/provider/products/add.js
@@ -43,11 +43,11 @@ const AddProduct = ({ store }) => {
     const file = e.target.files[0];
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "uploads");
+    formData.append("upload_preset", "gj3cvwjo");
     try {
       setLoading(true);
       const uploadRes = await axios.post(
-        "https://api.cloudinary.com/v1_1/dj7nomqfd/image/upload",
+        "https://api.cloudinary.com/v1_1/dg2mbrlin/image/upload",
         formData
       );
       setLoading(false);
@@ -89,70 +89,70 @@ const AddProduct = ({ store }) => {
     <React.Fragment>
       <Head>
         <title>Resume Builder | Profile Edit</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header heading={"Add Product"} />
-      <main className='relative -mt-40'>
-        <div className='w-[86%] mx-auto flex text-base text-left w-full md:my-8 md:align-middle'>
-          <div className='rounded-lg shadow w-full relative bg-white px-4 pt-14 pb-8 overflow-hidden sm:px-6 sm:pt-8 md:p-6 lg:p-8'>
+      <main className="relative -mt-40">
+        <div className="w-[86%] mx-auto flex text-base text-left w-full md:my-8 md:align-middle">
+          <div className="rounded-lg shadow w-full relative bg-white px-4 pt-14 pb-8 overflow-hidden sm:px-6 sm:pt-8 md:p-6 lg:p-8">
             <form
               onSubmit={submitHandler}
-              className='max-w-5xl mx-auto space-y-8 divide-y divide-gray-200 px-10'
+              className="max-w-5xl mx-auto space-y-8 divide-y divide-gray-200 px-10"
             >
-              <div className='space-y-6 sm:space-y-5'>
+              <div className="space-y-6 sm:space-y-5">
                 <div>
-                  <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">
                     Product Information
                   </h3>
-                  <p className='mt-1 max-w-2xl text-sm text-gray-500'>
+                  <p className="mt-1 max-w-2xl text-sm text-gray-500">
                     Use a permanent address where you can receive mail.
                   </p>
                 </div>
 
-                <div className='space-y-6 sm:space-y-5'>
-                  <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                <div className="space-y-6 sm:space-y-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                     <label
-                      htmlFor='firstName'
-                      className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'
+                      htmlFor="firstName"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
                       Name
                     </label>
-                    <div className='mt-1 sm:mt-0 sm:col-span-2'>
+                    <div className="mt-1 sm:mt-0 sm:col-span-2">
                       <input
-                        type='text'
-                        name='name'
-                        id='name'
+                        type="text"
+                        name="name"
+                        id="name"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
-                        autoComplete='given-name'
-                        className='max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
+                        autoComplete="given-name"
+                        className="max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
                   </div>
 
-                  <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                     <label
-                      htmlFor='price'
-                      className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'
+                      htmlFor="price"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
                       Price
                     </label>
-                    <div className='mt-1 sm:mt-0 sm:col-span-2'>
+                    <div className="mt-1 sm:mt-0 sm:col-span-2">
                       <input
-                        type='number'
-                        name='price'
-                        id='price'
+                        type="number"
+                        name="price"
+                        id="price"
                         value={price}
                         onChange={(e) => setPrice(e.target.value)}
-                        className='max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
+                        className="max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
                   </div>
 
-                  <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
                     <Dropdown
-                      heading='Categories'
+                      heading="Categories"
                       options={categories}
                       selected={selected}
                       setSelected={setSelected}
@@ -161,7 +161,7 @@ const AddProduct = ({ store }) => {
 
                   {selected.name === "Food" && (
                     <Variation
-                      title='Toppings'
+                      title="Toppings"
                       handleExtraOptions={(extra) =>
                         setVariations({ ...variations, toppings: [...variations.toppings, extra] })
                       }
@@ -179,7 +179,7 @@ const AddProduct = ({ store }) => {
 
                   {selected.name === "Clothes" && (
                     <Variation
-                      title='Sizes'
+                      title="Sizes"
                       handleExtraOptions={(extra) =>
                         setVariations({ ...variations, sizes: [...variations.sizes, extra] })
                       }
@@ -197,7 +197,7 @@ const AddProduct = ({ store }) => {
 
                   {selected.name === "Clothes" && (
                     <Variation
-                      title='Colors'
+                      title="Colors"
                       handleExtraOptions={(extra) =>
                         setVariations({ ...variations, colors: [...variations.colors, extra] })
                       }
@@ -215,7 +215,7 @@ const AddProduct = ({ store }) => {
 
                   {selected.name === "Medicines" && (
                     <Variation
-                      title='Doses'
+                      title="Doses"
                       handleExtraOptions={(extra) =>
                         setVariations({ ...variations, doses: [...variations.doses, extra] })
                       }
@@ -231,72 +231,72 @@ const AddProduct = ({ store }) => {
                     />
                   )}
 
-                  <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                     <label
-                      htmlFor='description'
-                      className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'
+                      htmlFor="description"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
                       Description
                     </label>
-                    <div className='mt-1 sm:mt-0 sm:col-span-2'>
+                    <div className="mt-1 sm:mt-0 sm:col-span-2">
                       {status === "loading" ? (
-                        <div className='animate-pulse'>
-                          <input className='max-w-lg block w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 cursor-not-allowed bg-gray-200 rounded-md h-10'></input>
+                        <div className="animate-pulse">
+                          <input className="max-w-lg block w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 cursor-not-allowed bg-gray-200 rounded-md h-10"></input>
                         </div>
                       ) : (
                         <textarea
-                          id='description'
-                          name='description'
-                          type='text'
+                          id="description"
+                          name="description"
+                          type="text"
                           value={description}
                           onChange={(e) => setDescription(e.target.value)}
-                          className='max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
+                          className="max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
                         />
                       )}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='pt-8 space-y-8 divide-y divide-gray-200 sm:space-y-5'>
+              <div className="pt-8 space-y-8 divide-y divide-gray-200 sm:space-y-5">
                 <div>
                   <div>
-                    <h3 className='text-lg leading-6 font-medium text-gray-900'>Product Image</h3>
-                    <p className='mt-1 max-w-2xl text-sm text-gray-500'>
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Product Image</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
                       This information will be displayed publicly so be careful what you share.
                     </p>
                   </div>
 
-                  <div className='mt-6 sm:mt-5 space-y-6 sm:space-y-5'>
-                    <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5'>
-                      <label htmlFor='photo' className='block text-sm font-medium text-gray-700'>
+                  <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+                    <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
+                      <label htmlFor="photo" className="block text-sm font-medium text-gray-700">
                         Photo
                       </label>
-                      <div className='mt-1 sm:mt-0 sm:col-span-2 '>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2 ">
                         {loading ? (
-                          <div className='animate-pulse'>
-                            <input className='appearance-none block w-3/4 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none bg-gray-200 sm:text-sm h-10'></input>
+                          <div className="animate-pulse">
+                            <input className="appearance-none block w-3/4 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none bg-gray-200 sm:text-sm h-10"></input>
                           </div>
                         ) : (
                           <input
-                            type='text'
+                            type="text"
                             value={image}
                             disabled={true}
                             onChange={(e) => setImage(e.target.value)}
-                            className='appearance-none block w-3/4 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                            className="appearance-none block w-3/4 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                           />
                         )}
                         {loading ? (
-                          <div className='inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm text-gray-500 cursor-not-allowed'>
-                            <Loader height='8' width='8' color='gray' />
+                          <div className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm text-gray-500 cursor-not-allowed">
+                            <Loader height="8" width="8" color="gray" />
                             Please Wait...
                           </div>
                         ) : (
                           <input
-                            className='mt-2 appearance-none block w-3/4 p-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-                            label='Choose File'
-                            type='file'
-                            name='image'
-                            id='profileImg'
+                            className="mt-2 appearance-none block w-3/4 p-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                            label="Choose File"
+                            type="file"
+                            name="image"
+                            id="profileImg"
                             onChange={uploadFileHandler}
                           />
                         )}
@@ -306,19 +306,19 @@ const AddProduct = ({ store }) => {
                 </div>
               </div>
 
-              <div className='pt-5'>
-                <div className='flex justify-end'>
-                  <Link href='/dashboard/student/profile'>
+              <div className="pt-5">
+                <div className="flex justify-end">
+                  <Link href="/dashboard/student/profile">
                     <button
-                      type='button'
-                      className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 '
+                      type="button"
+                      className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 "
                     >
                       Cancel
                     </button>
                   </Link>
                   <button
                     onClick={submitHandler}
-                    type='submit'
+                    type="submit"
                     disabled={loading}
                     className={`${
                       loading ? "cursor-not-allowed" : "hover:bg-blue-700"
